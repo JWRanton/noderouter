95cc2b4e967b0b7b9a73be8c0faffcc7d6396793
Justin Ranton <DeathStar@Justins-MacBook-Pro.local>
2016-06-03 23:35:05 -0500

    first commit

---
+++ hellonode.js
@@ -0,0 +1,11 @@
+'use strict';
+const http = require('http');
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end('<h1>Hello NodeJs</h1>');
+  })
+  .listen(3000, () => console.log('Server running on port 3000'));
+
+  
